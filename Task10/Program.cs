766d34534a972ee42c47278b9c2ace6eb1bea6a4
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-08-11 09:47:32 +0300

    Add project files.

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string a = Console.ReadLine();
+
+            Console.WriteLine(a[1]);
+            Console.ReadLine();
+        }
+    }
+}
